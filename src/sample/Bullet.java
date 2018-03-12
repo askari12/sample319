b7a8c279304c4de645ed6723e011e55ff1903494
Muhammad Askari Iqbal <gb.lespaul12@gmail.com>
2018-03-12 17:26:57 +0300

    Add files via upload

---
+++ src/sample/Bullet.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Bullet extends GameObject {
+    private int damage;
+
+    @Override
+    public void renderobject() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
