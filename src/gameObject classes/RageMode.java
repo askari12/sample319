fbfac500ef01a9dbab82e935e8463f0e20e95fa5
ZenoVita <kilicaslanalper@gmail.com>
2018-03-16 00:35:48 +0300

    same things

--- src/gameObject classes/RageMode.java
+++ src/gameObject classes/RageMode.java
@@ -1,9 +1,17 @@
 package sample;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 public class RageMode extends PowerUp {
-    public RageMode(Location loc, Dimension dimensions, Movement movement, Image img){
+    private Parent root;
+    private Circle rageMode;
+
+    public RageMode(Location loc, Dimension dimensions, Movement movement, Image img, Parent root){
         super(loc,dimensions,movement,img);
+        this.root = root;
     }
 
     @Override
@@ -15,7 +23,28 @@ public class RageMode extends PowerUp {
 
     @Override
     public void renderobject() {
+        rageMode = new Circle();
+        rageMode.setFill(new ImagePattern(this.img));
+        rageMode.setCenterX(loc.getX());
+        rageMode.setCenterY(loc.getY());
+        rageMode.setRadius(dimensions.getRadius());
 
+        ((Pane) root).getChildren().add(rageMode);
+    }
+    public int getX(){
+        return loc.getX();
+    }
+
+    public int getY(){
+        return loc.getY();
+    }
+
+    public void setX(int x) {
+        loc.setX(x);
+    }
+
+    public void setY(int y) {
+        loc.setY(y);
     }
 
     @Override
